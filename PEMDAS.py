1521f66c09b0546f732e9dc71e3b2a03889ea1cd
midkids <83714759+midkids@users.noreply.github.com>
2022-12-05 18:14:38 -0500

    Added parentheses logic plus game reset

--- PEMDAS.py
+++ PEMDAS.py
@@ -14,6 +14,7 @@ MODIFICATIONS BY VERSION:
 12/2/2022 - Laid out main window (no logic)
 12/3/2022 - Added essential logic
 12/4/2022 - Added comments, background color
+12/5/2022 - Added parentheses logic, reset game button
 """
 # imports
 from breezypythongui import EasyFrame
@@ -32,6 +33,7 @@ class PEMDAS(EasyFrame):
         # instance variables not associated with window components
         self.nbrOperators = 1   # the number of operators in expression
         self.expresssion = ""   # the expression being presented to user
+        self.evaluation = 0     # the evaluation expression (solution)
         self.attempts = 0       # the number of user attempts to solve expression
 
         # fonts
@@ -95,6 +97,10 @@ class PEMDAS(EasyFrame):
             background = "lightyellow",
             row = 8, column = 1, 
             sticky = "NSEW")
+        self.resetButton = self.addButton(text = "RESET", # reset game
+            row = 10, column = 1,
+            state = "normal",
+            command = self.resetGame)
     
         # add initial image - must be a GIF
         self.locked = PhotoImage(file = "lock2b.gif")   # create locked instance variable
@@ -110,6 +116,10 @@ class PEMDAS(EasyFrame):
         # no division operator to avoid decimals
         operators = ["+", "-", "*", "**"]
 
+        # Boolean to ensure only one exponentiation operator is 
+        # added to expression to keep resolution simple
+        usedExponent = False   
+
         buildExpression = str(randint(1,5)) # expression contents
         # add the appropriate number of operators base on past results
         for build in range(self.nbrOperators):  
@@ -118,11 +128,25 @@ class PEMDAS(EasyFrame):
             # to keep the solution manageable for user
             # other wise select an integer from 1-5, again to keep
             # the solution manageable for the user
-            buildExpression += operator
+            while operator == "**" and usedExponent:
+                operator = operators[randint(0,3)]
+            buildExpression += operator 
             if operator == "**":    
                 buildExpression += "2"
+                usedExponent = True
             else:    
                 buildExpression += str(randint(1,5))
+                evenOdd = randint(1,2)
+                print("evenOdd", evenOdd)
+                if evenOdd % 2 == 0 and \
+                    self.nbrOperators > 1 and \
+                    buildExpression[-3] != ")" and \
+                    buildExpression[-5:-3] != "**":
+                    expressionSlice1 = buildExpression[0:len(buildExpression)-3:]
+                    print("exp1", expressionSlice1)
+                    expressionSlice2 = buildExpression[-3:]
+                    print("exp2", expressionSlice2)
+                    buildExpression = expressionSlice1 + "(" + expressionSlice2 + ")"
         self.expressionText.setText(buildExpression)
         self.expression = buildExpression
         self.getButton["state"] = "disabled"
@@ -137,33 +161,31 @@ class PEMDAS(EasyFrame):
         user correctly solved the expresssion.
         """
         # eval function solves string expression
-        evaluation = eval(self.expression)
+        self.evaluation = eval(self.expression)
         try:
             userAnswer = self.answer.getNumber()    # get user solution
         except ValueError:
             self.messageBox(title = "ERROR",
                 message = "Answer must be an integer.")
         # if user solution correct
-        if evaluation == userAnswer:
-            self.locked = PhotoImage(file = "lock2a.gif")   # display unlocked lock
-            self.imageLabel["image"] = self.locked
-            self.messageBox(title = "CORRECT", 
-                message = "You solution is correct!")
+        if self.evaluation == userAnswer:
             self.correct()
         else:
-            self.messageBox(title = "INCORRECT", 
-                message = "You solution is incorrect.")
             self.incorrect()
     
     def correct(self):
         """
         The correct method awards the user points based
         on the number of attempts it took them to solve
-        the expression. It calls the reset method to reset
+        the expression. It calls the resetExpression method to reset
         the application for presenting the next expression
         to the user. It determines if the user has won
         the game.
         """
+        self.locked = PhotoImage(file = "lock2a.gif")   # display unlocked lock
+        self.imageLabel["image"] = self.locked
+        self.messageBox(title = "CORRECT", 
+            message = "Solution is correct!")
         self.attempts += 1  # increment number of user attempts  
         getScore = self.score.getNumber()   # get current user score
         if self.attempts == 3:
@@ -173,12 +195,19 @@ class PEMDAS(EasyFrame):
         else:
             getScore += 50
         self.score.setNumber(getScore)  # set updated user score
-        self.reset()    # reset application
-        self.nbrOperators += 1
+        self.resetExpression()    # reset expression
         # user wins game
-        if self.nbrOperators > 5:
-            self.messageBox(title = "WINNER", 
-                message = "You are now a PEMDAS expert!")
+        if self.nbrOperators == 4:
+            self.messageBox(title = "APPRENTICE", 
+                message = "You are now a PEMDAS+ apprentice!\nKeep going!")
+        if self.nbrOperators == 8:
+            self.messageBox(title = "EXPERT", 
+                message = "You are now a PEMDAS+ expert!\nWill you be a master?")
+        if self.nbrOperators > 10:
+            self.messageBox(title = "MASTER", 
+                message = "You are a PEMDAS+ master!\nGame will now reset!")
+            self.resetGame()
+        self.nbrOperators += 1
 
     def incorrect(self):
         """
@@ -187,35 +216,54 @@ class PEMDAS(EasyFrame):
         number of unsuccessful attempts, it decrements the
         number of user lives, it decrements the number of
         operators to be included in the next expression, and 
-        it calls the reset method to reset the application for 
-        presenting the next expression to the user. It 
+        it calls the resetExpression method to reset the 
+        expression for presentation to the user. It 
         determines if the user has lost the game.
         """
         self.attempts += 1  # increment number of user attempts  
         # user hit limit of incorrect solutions 
-        if self.attempts > 2:
+        if self.attempts == 3:
+            self.messageBox(title = "SOLUTION",
+            message = "The correct answer was: " + str(self.evaluation))
             getLives = self.lives.getNumber()
             getLives -= 1  
             self.lives.setNumber(getLives)
-            self.reset()    # reset application
+            self.resetExpression()    # reset expression
             if self.nbrOperators > 1:   # decrement number of operators
                 self.nbrOperators -= 1
             if getLives < 1:    # user lost game
-                self.messageBox(title = "BEGINNER", 
+                self.messageBox(title = "NOVICE", 
                 message = "Study up and try again!")
+        else:
+            if 3 - self.attempts > 1:
+                self.messageBox(title = "INCORRECT", 
+                    message = "Solution is incorrect. You have " + 
+                    str(3 - self.attempts) + " attempts left.")
+            else:
+                self.messageBox(title = "INCORRECT", 
+                    message = "Solution is incorrect. You have 1 attempt left.")
     
-    def reset(self):
+    def resetExpression(self):
         """
-        The reset method resets the application in
-        preparation for presenting the next expression
-        to the user.
+        The resetExpression method resets the 
+        expression for presentation to the user.
         """
         self.getButton["state"] = "normal"
         self.submitButton["state"] = "disabled"
         self.expression = ""
         self.expressionText.setText("")
         self.answer.setNumber(0)
-        self.attempts = 0        
+        self.attempts = 0
+
+    def resetGame(self):
+        """
+        The resetGame method resets the application
+        to allow the user to start completely over.
+        """
+        self.score.setNumber(0)
+        self.lives.setNumber(3)
+        self.nbrOperators = 1
+        self.resetExpression()       
 
 
 # main method
